bd2563ba5a3d3fdea1a47e76d36f0b1afea722bc
CaydenTan-310 <53048966+CaydenTan-310@users.noreply.github.com>
2019-10-18 16:26:34 +0800

    Update t01primeshcflcm_findab2.py

--- t01primeshcflcm_findab2.py
+++ t01primeshcflcm_findab2.py
@@ -1,2 +1,22 @@
 # If a and b are integers such that a * b = 18, find the possible values of a + b.
 
+multiplication = 18
+
+factors = []
+factors2 = []
+e=0
+for i in range(1, multiplication+1):
+    if multiplication % i == 0:
+        e+=1
+e=e/2
+for i in range(1, multiplication+1):
+    if multiplication % i == 0:
+        if (i <= e):
+            factors.append(i)
+        else:
+            factors2.append(i)
+
+e=0
+while e!=len(factors):
+    print(factors[e]+factors2[0-(e+1)])
+    e+=1
